6c9ed7bb81618f65969e4a4cc7dd548827237e6f
Lee Jong Min <9105lgm@xingxingmobility.com>
2020-02-22 18:30:13 +0900

    react-navigation setting

--- app/components/RootNavigate/index.js
+++ app/components/RootNavigate/index.js
@@ -2,16 +2,52 @@ import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SplashScene from './'
-const Navigate =  {
+import SplashScene from '../../scene/SplashScene'
+import LoginScene from '../../scene/LoginScene'
 
-    
-}
+// const RootStackNavigate = createStackNavigator(
+//     {
+//         Splash:{
+//             screen:SplashScene
+//         }
+//     },
+//     {
+//         initialRouteName: 'Splash', // 여기서 앱 버전체크가 이루어짐
+//     }
+// );
+// const RootNavigate = NavigationContainer(RootStackNavigate)
+// export default RootNavigate
 
-const RootStackNavigate = createStackNavigator({
-    Splash:{
-        screen:
-    }
-});
-const RootNavigate = NavigationContainer()
-export default RootNavigate
+const Stack = createStackNavigator();
+
+function RootNavigate() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator 
+            initialRouteName="Splash"
+            headerMode="screen"
+            screenOptions={{
+                headerTintColor: 'white',
+                headerStyle: { backgroundColor: 'tomato' },
+            }
+        }>
+        <Stack.Screen 
+            name="Splash"
+            component={SplashScene}
+            options={{
+                headerShown:false
+            }}
+        />
+        <Stack.Screen 
+            name="Login"
+            component={LoginScene}
+            options={{
+                headerShown:true
+            }}
+        />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+export default RootNavigate;
