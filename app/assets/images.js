ae436e666244df8bea5ec6ec539d65fc70dc709e
Lee Jong Min <9105lgm@xingxingmobility.com>
2020-02-27 21:54:59 +0900

    이미지 asset 추가

---
+++ app/assets/images.js
@@ -0,0 +1,6 @@
+const images = {
+    imgSplash:require('./images/imgSplash/imgSplash.png'),
+    iconMypage:require('./images/iconMypage/iconMypage.png'),
+    selectArrow:require('./images/selectArrow/selectArrow.png'),
+};
+export default images;
