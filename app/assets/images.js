d0edb19b4e5aff45778c9cdc30d8eace611230e3
Lee Jong Min <9105lgm@xingxingmobility.com>
2020-02-28 03:12:11 +0900

    달, 마크 추가

--- app/assets/images.js
+++ app/assets/images.js
@@ -2,5 +2,8 @@ const images = {
     imgSplash:require('./images/imgSplash/imgSplash.png'),
     iconMypage:require('./images/iconMypage/iconMypage.png'),
     selectArrow:require('./images/selectArrow/selectArrow.png'),
+    imgMonth:require('./images/imgMonth/imgMonth.png'),
+    imgFollowDark:require('./images/imgFollowDark/imgFollowDark.png'),
+    imgFollow:require('./images/imgFollow/imgFollow.png'),
 };
 export default images;
