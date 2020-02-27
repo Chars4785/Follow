46c5ad56add3e18cda4b860b1f04a8db8b83dc5f
Lee Jong Min <9105lgm@xingxingmobility.com>
2020-02-27 21:55:51 +0900

    textStyle 추가

---
+++ app/assets/styles/textStyles.js
@@ -0,0 +1,19 @@
+import StyleConfig from '../../config/StyleConfig.js';
+
+const textStyles = {
+    boldText: {
+        fontFamily: StyleConfig.BOLD_FONT,
+    },
+    regularText: {
+        fontFamily: StyleConfig.REGULAR_FONT,
+    },
+    lightText: {
+        fontFamily: StyleConfig.LIGHT_FONT,
+    },
+    mediumText: {
+        fontFamily: StyleConfig.REGULAR_FONT,
+        //fontFamily: StyleConfig.MEDIUM_FONT,
+    },
+};
+
+export default textStyles;
