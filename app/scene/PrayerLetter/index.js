ecacdb2a969fa48b12504ca33ebdbdc5d4fd9ace
Lee Jong Min <9105lgm@xingxingmobility.com>
2020-03-04 19:02:40 +0900

    초기 구상

---
+++ app/scene/PrayerLetter/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const PrayerLetter = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>PrayerLetter</Text>
+        </View>
+    )
+}
+
+export default PrayerLetter
