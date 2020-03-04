bfe36ef9e309c1492c4c38bdfdcb407395dfab06
Lee Jong Min <9105lgm@xingxingmobility.com>
2020-03-04 19:00:30 +0900

    이미지 추가

--- app/assets/images.js
+++ app/assets/images.js
@@ -5,5 +5,7 @@ const images = {
     imgMonth:require('./images/imgMonth/imgMonth.png'),
     imgFollowDark:require('./images/imgFollowDark/imgFollowDark.png'),
     imgFollow:require('./images/imgFollow/imgFollow.png'),
+    iconYoutube:require('./images/iconYoutube/iconYoutube.png'),
+    iconInstagram:require('./images/iconInstagram/iconInstagram.png'),
 };
 export default images;
