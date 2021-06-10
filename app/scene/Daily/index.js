5fae8b7fa2b20e99e5b812a25321df073dee8d6e
Lee Jong Min <9105lgm@naver.com>
2021-06-10 10:41:27 +0900

    마이페이지 추가

--- app/scene/Daily/index.js
+++ app/scene/Daily/index.js
@@ -1,14 +1,87 @@
-import { inject } from 'mobx-react'
-import React, {useEffect} from 'react'
-import { View, Text } from 'react-native'
-
+import React, {useEffect,useState} from 'react'
+import { View, Text, TouchableOpacity, StyleSheet,ScrollView,Image,Linking,AppState, SafeAreaView, TextInput } from 'react-native'
+import { getWidthScaledValue as wsv, getHeightScaledValue as hsv } from '../../api/util';
+import Images from '@assets/images'
+import textStyles from '../../assets/styles/textStyles'
+import moment from 'moment';
+import { inject } from 'mobx-react';
+import images from '../../assets/images';
+import Swiper from 'react-native-swiper'
+import { useFocusEffect, useIsFocused } from '@react-navigation/native';
+const loginPageTitle =
+`
+이곳은 팔로우 공동체
+지체들의 공간입니다.
+`
 const Daily = (props) => {
-  console.log(props)
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Daily</Text>
-        </View>
+    return (    
+        <SafeAreaView style={styles.loginRoot}>
+            <Image style={styles.loginPageLogoStyle} source={Images.loginPageLogo} />
+            <View style={styles.loginPageTitleStyle}>
+                <Text style={styles.loginTitleText}>{loginPageTitle}</Text>
+            </View>
+            <View style={styles.loginViewWrapper}>
+                <Text style={styles.loginInputTitleText}>username</Text>
+                <TextInput 
+                    style={styles.loginTextInputStyle}
+                />
+            </View>
+            <View style={styles.loginViewWrapper}>
+                <Text style={styles.loginInputTitleText}>password</Text>
+                <TextInput
+                    style={styles.loginTextInputStyle}
+                />
+            </View>
+            <View style={styles.loginButton}>
+                <Text style={styles.loginButtonText}>로그인 하기</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
 export default Daily
+
+const styles = StyleSheet.create({
+    loginRoot:{
+        justifyContent:'center',
+        alignItems:'center',
+        paddingHorizontal:wsv(82)
+    },
+    loginPageLogoStyle:{
+        marginTop:wsv(110),
+        width:wsv(189),
+        height:wsv(189)
+    },
+    loginPageTitleStyle:{
+
+    },
+    loginTitleText:{
+        ...textStyles.mediumText,
+        fontSize:wsv(36)
+    },
+    loginViewWrapper:{
+        marginTop:wsv(36)
+    },
+    loginInputTitleText:{
+        fontSize:wsv(26)
+    },
+    loginTextInputStyle:{
+        marginTop:wsv(15),
+        width:wsv(582),
+        height:wsv(90),
+        backgroundColor:'white'
+    },
+    loginButton:{
+        height:wsv(113),
+        width:wsv(582),
+        marginTop:wsv(66),
+        backgroundColor:'#e63258',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    loginButtonText:{
+        ...textStyles.boldText,
+        fontSize:wsv(30),
+        color:'white'
+    }
+})
--- app/scene/MyPage/index.js
+++ app/scene/MyPage/index.js
@@ -1,12 +1,106 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useEffect,useState} from 'react'
+import { View, Text, TouchableOpacity, StyleSheet,ScrollView,Image,Linking,AppState, SafeAreaView, TextInput } from 'react-native'
+import { getWidthScaledValue as wsv, getHeightScaledValue as hsv } from '../../api/util';
+import Images from '@assets/images'
+import textStyles from '../../assets/styles/textStyles'
+import moment from 'moment';
+import { inject } from 'mobx-react';
+import images from '../../assets/images';
+import Swiper from 'react-native-swiper'
+import { useFocusEffect, useIsFocused } from '@react-navigation/native';
 
 const MyPage = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <SafeAreaView style={styles.root}>
+            <View style={styles.infoFristWrapper}>
+                <View style={styles.infoBoxStyle}>
+                    <View style={styles.infoTitleStyle}>
+                        <Text style={styles.infoTitleText}>이름</Text>
+                    </View>
+                    <View style={styles.infoContentStyle}>
+                        <Text style={styles.infoContentText}>박지원</Text>
+                    </View>
+                </View>
+                <View style={styles.infoBoxStyle}>
+                    <View style={styles.infoTitleStyle}>
+                        <Text style={styles.infoTitleText}>생년월일</Text>
+                    </View>
+                    <View style={styles.infoContentStyle}>
+                        <Text style={styles.infoContentText}>94/12/23</Text>
+                    </View>
+                </View>
+                <View style={styles.infoBoxStyle}>
+                    <View style={styles.infoTitleStyle}>
+                        <Text style={styles.infoTitleText}>휴대폰 번호</Text>
+                    </View>
+                    <View style={styles.infoContentStyle}>
+                        <Text style={styles.infoContentText}>010 9074 0000</Text>
+                    </View>
+                </View>
+                <View style={styles.infoBoxStyle}>
+                    <View style={styles.infoTitleStyle}>
+                        <Text style={styles.infoTitleText}>아이디</Text>
+                    </View>
+                    <View style={styles.infoContentStyle}>
+                        <Text style={styles.infoContentText}>jiwon park</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.infoSecondWrapper}>
+                <View style={styles.infoBoxStyle}>
+                    <View style={styles.infoTitleStyle}>
+                        <Text style={styles.infoTitleText}>리더</Text>
+                    </View>
+                    <View style={styles.infoContentStyle}>
+                        <Text style={styles.infoContentText}>이순종</Text>
+                    </View>
+                </View>
+                <View style={styles.infoBoxStyle}>
+                    <View style={styles.infoTitleStyle}>
+                        <Text style={styles.infoTitleText}>코디조</Text>
+                    </View>
+                    <View style={styles.infoContentStyle}>
+                        <Text style={styles.infoContentText}>박요한</Text>
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
 export default MyPage
+
+const styles = StyleSheet.create({
+    root:{
+
+    },
+    infoFristWrapper:{
+        marginTop:wsv(340),
+    },
+    infoBoxStyle:{
+        flexDirection:'row',
+        backgroundColor:'white',
+        height:wsv(109),
+        width:'100%'
+    },
+    infoTitleStyle:{
+        width:wsv(248),
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    infoTitleText:{
+        color:'#757575',
+        fontSize:wsv(30)
+    },
+    infoContentStyle:{
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    infoContentText:{
+        ...textStyles.mediumText,
+        fontSize:wsv(34)
+    },
+    infoSecondWrapper:{
+        marginTop:wsv(47)
+    }
+})
